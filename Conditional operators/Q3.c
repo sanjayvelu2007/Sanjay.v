6f6a7aecec3cccf3d126b7b582364f8a76fa9884
sanjayvelu2007 <sanjayvelu2007@gmail.com>
2025-09-08 21:36:33 -0700

    Add files via upload

---
+++ Conditional operators/Q3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int a=10;
+	int b=30;
+	if((a>b)&&(b>a))
+	{
+	printf("a is bigger");
+	}
+	else
+	{
+		printf("b is bigger");
+    }
+}
---
+++ Conditional operators/Q4.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int a=30;
+	int b=10;
+	if((a<b)&&(b<a))
+	{
+	printf("a is smaller");
+	}
+	else
+	{
+		printf("b is smaller");
+    }
+}
